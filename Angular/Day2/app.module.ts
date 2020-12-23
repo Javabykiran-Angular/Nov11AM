e67cc93c49c442ec60ad65173a8b1ffe96c03fcb
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-12-23 17:38:04 +0530

    Day2

---
+++ Angular/Day2/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentComponent } from './student.component';
+import { EmployeeComponent } from './employee.component';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentComponent,
+    EmployeeComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
